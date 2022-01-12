988d4bba0f6137642bd0e2cb9fdff0dffb867a19
gaaasp <gaspard.lasorne@protonmail.com>
2022-01-12 12:56:26 +0100

    Properly add Inter font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,18 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Inter"
-						rel="stylesheet"
-					/>
-				</Head>
-				<body className="bg-background text-foreground">
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
-
-export default MyDocument;
